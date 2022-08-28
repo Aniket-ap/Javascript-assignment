adab24276ade363d4dcde780139c410112aa2510
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-28 21:46:35 +0530

    assignments 6 to 10 added

---
+++ 07_assignment.js
@@ -0,0 +1,4 @@
+// 7. Use lastIndexOf to determine the position of the last occurrence of a script.
+
+var getIndex = "Bhart mata ki jai";
+console.log(getIndex.lastIndexOf("a"));
---
+++ 10_assignment.js
@@ -0,0 +1,14 @@
+// 10. Boolean value is either true or false.
+    // - Write three JavaScript statement which provide truthy value.
+    // - Write three JavaScript statement which provide falsy value.
+
+console.log(true ? "True" : "False");
+console.log(1 ? "True" : "False");
+console.log("truthy" ? "True" : "False");
+
+console.log(false ? "True" : "False");
+console.log("" ? "True" : "False");
+console.log(null ? "True" : "False");
+console.log(0 ? "True" : "False");
+console.log(undefined ? "True" : "False");
+
