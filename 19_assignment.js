02aa83a5d9fae1277c1924ff6b8ecdfa624f7178
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-30 18:09:05 +0530

    assignment done

---
+++ 19_assignment.js
@@ -0,0 +1,8 @@
+// 19. Even numbers are divisible by 2 and the remainder is zero. 
+// How do you check, if a number is even or not using JavaScript?
+
+let number = 11;
+
+let ans = number % 2 == 0 ? true : false;
+
+console.log(ans);
