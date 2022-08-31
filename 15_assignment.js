567506735678b4c0ac94ad93ded9a578d6e0d29a
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-31 18:59:21 +0530

    15 assignment done

--- 15_assignment.js
+++ 15_assignment.js
@@ -1,2 +1,10 @@
 // 15. Calculate the slope, x-intercept and y-intercept of y = 2x -2
 
+function calcSlope(x) {
+    let y = (2 * x) - 2;
+
+    return y;
+}
+
+let res = calcSlope(3);
+console.log(res);
